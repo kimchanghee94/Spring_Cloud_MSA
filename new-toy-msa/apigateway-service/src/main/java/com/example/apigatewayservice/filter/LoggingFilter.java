e0bb709ccb68d3592b5a90338c697e56b53dc8d1
kimchanghee <kimchanghee94@naver.com>
2025-12-27 02:36:47 +0900

    #main 1. Filter 우선순위 처리 실습

--- new-toy-msa/apigateway-service/src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
+++ new-toy-msa/apigateway-service/src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
@@ -3,7 +3,9 @@ package com.example.apigatewayservice.filter;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -16,9 +18,32 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
         super(Config.class);
     }
 
+//    @Override
+//    public GatewayFilter apply(Config config){
+//        return (exchange, chain) -> {
+//            ServerHttpRequest request = exchange.getRequest();
+//            ServerHttpResponse response = exchange.getResponse();
+//
+//            //Custom Pre Filter
+//            log.info("Logging Filter baseMessage: {}, {}", config.getBaseMessage(), request.getRemoteAddress());
+//
+//            if(config.isPreLogger()){
+//                log.info("Logging Filter Start: request uri -> {}", request.getURI());
+//            }
+//
+//            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+//                //Custom Post Filter
+//                if(config.isPostLogger()){
+//                    log.info("Logging Filter End: response code -> {}", response.getStatusCode());
+//                }
+//            }));
+//        };
+//    }
+
+    /*우선순위를 갖는 Logging Filter 적용*/
     @Override
     public GatewayFilter apply(Config config){
-        return (exchange, chain) -> {
+        GatewayFilter filter = new OrderedGatewayFilter((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
 
@@ -35,7 +60,8 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
                     log.info("Logging Filter End: response code -> {}", response.getStatusCode());
                 }
             }));
-        };
+        }, Ordered.HIGHEST_PRECEDENCE);
+        return filter;
     }
 
     @Data
