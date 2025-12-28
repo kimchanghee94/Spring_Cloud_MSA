43077918c7d4a9ed382f7d6d4dcba269c1a215e4
kimchanghee <kimchanghee94@naver.com>
2025-12-28 21:18:05 +0900

    #main 1. user-service 프로젝트 생성 2. h2 DB 연동

--- new-toy-msa/user-service_section2/src/main/java/com/example/userservice/UserServiceApplication.java
+++ new-toy-msa/user-service_section2/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,12 +1,8 @@
 package com.example.userservice;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -15,10 +11,5 @@ public class UserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
-	@Bean
-	CommandLineRunner runner(DataSource dataSource){
-		return args -> {
-			System.out.println("URL: " + dataSource.getConnection().getMetaData().getURL());
-		};
-	}
+
 }
