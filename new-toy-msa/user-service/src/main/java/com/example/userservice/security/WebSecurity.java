165337e39a5a2773280ee536533360420a2e230b
kimchanghee <kimchanghee94@naver.com>
2025-12-31 16:49:00 +0900

    #main 1. actuator 실습

--- new-toy-msa/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ new-toy-msa/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -38,6 +38,7 @@ public class WebSecurity {
         http.csrf( (csrf) -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/h2-console/**").permitAll()  // 특정 경로 허용
+                        .requestMatchers("/actuator/**").permitAll()  // 특정 경로 허용
                         .requestMatchers("/**").access(
                                 new WebExpressionAuthorizationManager(
                                         "hasIpAddress('127.0.0.1') or hasIpAddress('::1') or " +
